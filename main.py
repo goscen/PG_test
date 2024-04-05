3a73592255bd7c30fce802f9f27013ad151b70f7
goscen <anar.guseynov.03@mail.ru>
2024-04-06 01:11:02 +0500

    Basic logic

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
+from argparse import ArgumentParser
+import time
+from getpass import getpass
+from socket import gethostbyname
 import paramiko
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def install(hostname: str, use_pass: str, username: str, port: int):
+    with paramiko.SSHClient() as cli:
+        cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        if use_pass == "yes":
+            password = getpass("Enter password for host")
+            cli.connect(hostname=hostname, username=username,
+                        password=password, allow_agent=False, look_for_keys=False, port=port)
+        else:
+            cli.connect(hostname=hostname, username=username, allow_agent=True, look_for_keys=True, port=port)
+        ssh = cli.invoke_shell()
+        ssh.send("env\n")
+        time.sleep(1)
+        print(ssh.recv(1000))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('example.com', username='your_username', password='your_password')
+    parser = ArgumentParser(description="Install postgresql DB on remote host")
+    parser.add_argument("destination", type=str, help="Enter host(ip/hostname)")
+    parser.add_argument("-p", type=str, default="no", required=False, choices=["yes", "no"],
+                        help="Use password to connect remote host")
+    parser.add_argument("-P", type=int, default=22, required=False, help="Enter ssh port")
+    parser.add_argument("-u", type=str, required=True, help="Enter username")
+    args = parser.parse_args()
+    try:
+        ip = gethostbyname(args.destination)
+    except Exception as err:
+        raise Exception("wrong host or ip")
+    install(args.destination, args.p, args.u, abs(args.P))
